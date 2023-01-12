f2d86a380c80238f9ddf48e57f3db9d17c86ace0
Achmad Amri <achmad.amri@gmail.com>
2023-01-12 15:13:02 +0700

    update

--- bsd-front-rec/src/app/pages/jobs-detail/jobs-detail.component.ts
+++ bsd-front-rec/src/app/pages/jobs-detail/jobs-detail.component.ts
@@ -32,10 +32,11 @@ export class JobsDetailComponent implements OnInit {
   postAddJobResponse: PostAddJobResponse = new PostAddJobResponse();
   getJobRequest: GetJobRequest = new GetJobRequest();
   getJobResponse: GetJobResponse = new GetJobResponse();
-  selectedFile: File;
+  selectedFiles: File[] = [];
   postUploadResumeRequest: PostUploadResumeRequest = new PostUploadResumeRequest();
   postUploadResumeResponse: PostUploadResumeResponse = new PostUploadResumeResponse();
   uploadPercentage = 0;
+  totalUpload = 0;
 
   constructor(
     private route: ActivatedRoute,
@@ -44,27 +45,27 @@ export class JobsDetailComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.pageEvent = this.util.cachePaginator('jobs-detail.pageEvent');    
+    this.pageEvent = this.util.cachePaginator('jobs-detail.pageEvent');
 
     this.route.paramMap.subscribe(params => {
       this.postAddJobRequest.tbJob.tbjUuid = params.get('tbjUuid');
 
       if (this.postAddJobRequest.tbJob.tbjUuid != '0') {
         this.jobService.getJob(this.postAddJobRequest.tbJob.tbjUuid)
-        .subscribe(
-          successResponse => {
-            this.getJobResponse = successResponse;
+          .subscribe(
+            successResponse => {
+              this.getJobResponse = successResponse;
 
-            this.postAddJobRequest.tbJob.tbjId = this.getJobResponse.tbJob.tbjId;
-            this.postAddJobRequest.tbJob.tbjUuid = this.getJobResponse.tbJob.tbjUuid;
-            this.postAddJobRequest.tbJob.tbjName = this.getJobResponse.tbJob.tbjName;
-            this.postAddJobRequest.tbJob.tbjStatus = this.getJobResponse.tbJob.tbjStatus;
-          },
-          errorResponse => {            
-            this.getJobResponse = new GetJobResponse();
-            this.util.showNotification('danger', 'top', 'center', errorResponse.error.message);
-          }
-        );
+              this.postAddJobRequest.tbJob.tbjId = this.getJobResponse.tbJob.tbjId;
+              this.postAddJobRequest.tbJob.tbjUuid = this.getJobResponse.tbJob.tbjUuid;
+              this.postAddJobRequest.tbJob.tbjName = this.getJobResponse.tbJob.tbjName;
+              this.postAddJobRequest.tbJob.tbjStatus = this.getJobResponse.tbJob.tbjStatus;
+            },
+            errorResponse => {
+              this.getJobResponse = new GetJobResponse();
+              this.util.showNotification('danger', 'top', 'center', errorResponse.error.message);
+            }
+          );
 
         this.saveUpdate = 'Update';
       } else {
@@ -82,72 +83,79 @@ export class JobsDetailComponent implements OnInit {
   }
 
   search() {
-    
+
   }
 
   clear() {
-    
+
   }
 
   saveupdate() {
     this.clicked = !this.clicked;
 
     this.jobService.postAddJob(this.postAddJobRequest)
-    .subscribe(
-      successResponse => {
-        this.clicked = !this.clicked;
-        this.postAddJobResponse = successResponse;
-        this.util.showNotification('info', 'top', 'center', successResponse.message);
-        this.router.navigate(['/jobs' + '/' + this.postAddJobResponse.tbJob.tbjUuid]);
-      },
-      errorResponse => {
-        this.clicked = !this.clicked;
-        this.postAddJobResponse = new PostAddJobResponse();
-        this.util.showNotification('danger', 'top', 'center', errorResponse.error.message);
-      }
-    );
+      .subscribe(
+        successResponse => {
+          this.clicked = !this.clicked;
+          this.postAddJobResponse = successResponse;
+          this.util.showNotification('info', 'top', 'center', successResponse.message);
+          this.router.navigate(['/jobs' + '/' + this.postAddJobResponse.tbJob.tbjUuid]);
+        },
+        errorResponse => {
+          this.clicked = !this.clicked;
+          this.postAddJobResponse = new PostAddJobResponse();
+          this.util.showNotification('danger', 'top', 'center', errorResponse.error.message);
+        }
+      );
   }
 
   onFileChanged(event) {
-    this.selectedFile = event.target.files[0];
+    for (const file of event.target.files) {
+      this.selectedFiles.push(file);
+    }
   }
 
   upload() {
     this.clicked = !this.clicked;
 
-    if (this.selectedFile == null) {
+    if (this.selectedFiles.length === 0) {
       this.clicked = !this.clicked;
-      this.util.showNotification('info', 'top', 'center', 'No file selected');      
+      this.util.showNotification('info', 'top', 'center', 'No files selected');
     } else {
-      const reader = new FileReader();
-      reader.readAsDataURL(this.selectedFile);  
-      this.util.showNotification('info', 'top', 'center', 'Uploading ' + this.selectedFile.name);
-  
-      this.jobService.postUploadResume(this.postUploadResumeRequest, this.selectedFile)
-        .subscribe(
-          successResponse => {
-            if (successResponse.type === HttpEventType.UploadProgress) {
-              this.uploadPercentage = Math.round(100 * successResponse.loaded / successResponse.total);
-            } else if (successResponse.type === HttpEventType.Response) {
-                this.clicked = !this.clicked;
-                this.uploadPercentage = 0;
-                this.fileInput.nativeElement.value = '';
+      this.util.showNotification('info', 'top', 'center', 'Uploading ' + this.selectedFiles.length + ' files');
+      this.totalUpload = 0;
+      for (const file of this.selectedFiles) {
+        this.jobService.postUploadResume(this.postUploadResumeRequest, file)
+          .subscribe(
+            successResponse => {
+              if (successResponse.type === HttpEventType.UploadProgress) {
+                this.uploadPercentage = Math.round(100 * successResponse.loaded / successResponse.total);
+              } else if (successResponse.type === HttpEventType.Response) {
+                this.totalUpload++;
+
                 this.postUploadResumeResponse = successResponse.body;
                 this.util.showNotification('info', 'top', 'center', this.postUploadResumeResponse.message);
+
+                if (this.totalUpload == this.selectedFiles.length) {
+                  this.clicked = !this.clicked;
+                  this.selectedFiles = [];
+                  this.fileInput.nativeElement.value = '';
+                }
+              }
+            },
+            errorResponse => {
+              this.clicked = !this.clicked;
+              this.selectedFiles = [];
+              this.fileInput.nativeElement.value = '';
+              this.postUploadResumeResponse = new PostUploadResumeResponse();
+              if (errorResponse.status == 0) {
+                this.util.showNotification('danger', 'top', 'center', errorResponse.statusText);
+              } else {
+                this.util.showNotification('danger', 'top', 'center', errorResponse.error.message);
+              }
             }
-          },
-          errorResponse => {            
-            this.clicked = !this.clicked;
-            this.postUploadResumeResponse = new PostUploadResumeResponse();
-            if (errorResponse.status == 0) {
-              this.util.showNotification('danger', 'top', 'center', errorResponse.statusText);
-            } else {
-              this.util.showNotification('danger', 'top', 'center', errorResponse.error.message);              
-            }
-            this.uploadPercentage = 0;
-            this.fileInput.nativeElement.value = '';
-          }
-        );
+          );
+      }
     }
   }
 
