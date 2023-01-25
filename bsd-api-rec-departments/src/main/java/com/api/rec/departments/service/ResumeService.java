28f813a93ab0afc350f5d9593e532d5a5402cac0
Achmad Amri <achmad.amri@gmail.com>
2023-01-25 22:28:22 +0700

    update

--- bsd-api-rec-departments/src/main/java/com/api/rec/departments/service/ResumeService.java
+++ bsd-api-rec-departments/src/main/java/com/api/rec/departments/service/ResumeService.java
@@ -4,6 +4,7 @@ import java.nio.ByteBuffer;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.LinkedHashMap;
@@ -29,10 +30,18 @@ import com.affinda.api.client.models.ResumeRequestBody;
 import com.api.rec.departments.db.entity.TbJob;
 import com.api.rec.departments.db.entity.TbJobResume;
 import com.api.rec.departments.db.entity.TbResume;
+import com.api.rec.departments.db.entity.TbResumeCertification;
+import com.api.rec.departments.db.entity.TbResumeEducation;
+import com.api.rec.departments.db.entity.TbResumeSkill;
+import com.api.rec.departments.db.entity.TbResumeWorkExperience;
 import com.api.rec.departments.db.entity.TbUser;
 import com.api.rec.departments.db.repository.TbJobRepository;
 import com.api.rec.departments.db.repository.TbJobResumeRepository;
+import com.api.rec.departments.db.repository.TbResumeCertificationRepository;
+import com.api.rec.departments.db.repository.TbResumeEducationRepository;
 import com.api.rec.departments.db.repository.TbResumeRepository;
+import com.api.rec.departments.db.repository.TbResumeSkillRepository;
+import com.api.rec.departments.db.repository.TbResumeWorkExperienceRepository;
 import com.api.rec.departments.db.repository.TbUserRepository;
 import com.api.rec.departments.model.resume.GetResumeListRequestModel;
 import com.api.rec.departments.model.resume.GetResumeListResponseModel;
@@ -45,6 +54,10 @@ import com.api.rec.departments.model.resume.PostUploadResumeResponseModel;
 import com.api.rec.departments.util.TokenUtil;
 import com.api.rec.departments.util.Uid;
 import com.azure.core.credential.TokenCredential;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 
 import reactor.core.publisher.Flux;
 
@@ -70,7 +83,21 @@ public class ResumeService {
 	@Autowired
 	private TbJobResumeRepository tbJobResumeRepository;
 
+	@Autowired
+	private TbResumeEducationRepository tbResumeEducationRepository;
+
+	@Autowired
+	private TbResumeWorkExperienceRepository tbResumeWorkExperienceRepository;
+
+	@Autowired
+	private TbResumeSkillRepository tbResumeSkillRepository;
+
+	@Autowired
+	private TbResumeCertificationRepository tbResumeCertificationRepository;
+
 	public TbResume affindaCreateResume(TbUser tbUser, MultipartFile file) throws Exception {
+		Gson gson = new Gson();
+
 		String apiKey = env.getProperty("key.affinda");
 		TokenCredential credential = new AffindaTokenCredential(apiKey);
 		AffindaAPI client = new AffindaAPIBuilder().credential(credential).buildClient();
@@ -103,18 +130,18 @@ public class ResumeService {
 		}
 
 		if (data != null) {
-			tbResume.setTbrDataPhoneNumbers(((ArrayList) data.get("phoneNumbers")).toString());
-			tbResume.setTbrDataWebsites(((ArrayList) data.get("websites")).toString());
-			tbResume.setTbrDataEmails(((ArrayList) data.get("emails")).toString());
+			tbResume.setTbrDataPhoneNumbers(gson.toJson(data.get("phoneNumbers")));
+			tbResume.setTbrDataWebsites(gson.toJson(data.get("websites")));
+			tbResume.setTbrDataEmails(gson.toJson(data.get("emails")));
 			tbResume.setTbrDataDateOfBirth((String) data.get("dateOfBirth"));
 			tbResume.setTbrDataObjective((String) data.get("objective"));
-			tbResume.setTbrDataLanguages(((ArrayList) data.get("languages")).toString());
+			tbResume.setTbrDataLanguages(gson.toJson(data.get("languages")));
 			tbResume.setTbrDataSummary((String) data.get("summary"));
 			tbResume.setTbrDataTotalYearsExperience((Integer) data.get("totalYearsExperience"));
 			tbResume.setTbrDataProfession((String) data.get("profession"));
 			tbResume.setTbrDataLinkedin((String) data.get("linkedin"));
-			tbResume.setTbrDataCertifications(((ArrayList) data.get("certifications")).toString());
-			tbResume.setTbrDataPublications(((ArrayList) data.get("publications")).toString());
+			tbResume.setTbrDataCertifications(gson.toJson(data.get("certifications")));
+			tbResume.setTbrDataPublications(gson.toJson(data.get("publications")));
 			tbResume.setTbrDataIsResumeProbability((Integer) data.get("isResumeProbability"));
 		}
 
@@ -132,19 +159,19 @@ public class ResumeService {
 		}
 		
 		if (dataEducation != null) {
-			tbResume.setTbrDataEducation(dataEducation.toString());
+			tbResume.setTbrDataEducation(gson.toJson(dataEducation));
 		}
 		
 		if (dataWorkExperience != null) {
-			tbResume.setTbrDataWorkExperience(dataWorkExperience.toString());
+			tbResume.setTbrDataWorkExperience(gson.toJson(dataWorkExperience));
 		}
 
 		if (dataSkills != null) {
-			tbResume.setTbrDataSkills(dataSkills.toString());
+			tbResume.setTbrDataSkills(gson.toJson(dataSkills));
 		}
 		
 		if (dataReferees != null) {
-			tbResume.setTbrDataReferees(dataReferees.toString());
+			tbResume.setTbrDataReferees(gson.toJson(dataReferees));
 		}
 		
 		if (meta != null) {
@@ -186,7 +213,100 @@ public class ResumeService {
 				String fileName = responseModel.getResponseId() + "-" + StringUtils.cleanPath(file.getOriginalFilename());
 				Files.copy(file.getInputStream(), Paths.get(env.getProperty("file.upload.dir") + fileName), StandardCopyOption.REPLACE_EXISTING);
 	
-				TbResume tbResume = affindaCreateResume(optTbUser.get(), file);
+				TbResume tbResume = affindaCreateResume(optTbUser.get(), file);				
+				tbResume.setTbrDataPhoneNumbers(getJsonArray(tbResume.getTbrDataPhoneNumbers()));
+				tbResume.setTbrDataEmails(getJsonArray(tbResume.getTbrDataEmails()));
+				tbResume.setTbrDataWebsites(getJsonArray(tbResume.getTbrDataWebsites()));
+				tbResume.setTbrDataLanguages(getJsonArray(tbResume.getTbrDataLanguages()));
+				tbResumeRepository.save(tbResume);
+				
+				String jsonEducation = tbResume.getTbrDataEducation();
+				JsonArray jsonArrayEducation = JsonParser.parseString(jsonEducation).getAsJsonArray();
+				for (JsonElement jsonElement : jsonArrayEducation) {
+					TbResumeEducation tbResumeEducation = new TbResumeEducation();
+					tbResumeEducation.setTbreUuid(new Uid().generateString(5));
+					tbResumeEducation.setTbreCreateId(optTbUser.get().getTbuId());
+					tbResumeEducation.setTbreCreateIdc(optTbUser.get().getTbuCreateIdc());
+					tbResumeEducation.setTbreCreateDate(new Date());
+					tbResumeEducation.setTbreStatus(TbResumeEducationRepository.Active);
+					tbResumeEducation.setTbrId(tbResume.getTbrId());
+					tbResumeEducation.setTbreOrganization(getJson(jsonElement, "organization"));
+					tbResumeEducation.setTbreEducation(getJson(jsonElement, "accreditation.education"));
+					tbResumeEducation.setTbreEducationLevel(getJson(jsonElement, "accreditation.educationLevel"));
+					tbResumeEducation.setTbreInputStr(getJson(jsonElement, "accreditation.inputStr"));
+					tbResumeEducation.setTbreMatchStr(getJson(jsonElement, "accreditation.matchStr"));
+					tbResumeEducation.setTbreGradeRaw(getJson(jsonElement, "grade.raw"));
+					tbResumeEducation.setTbreGradeValue(getJson(jsonElement, "grade.value"));
+					tbResumeEducation.setTbreGradeMetric(getJson(jsonElement, "grade.metric"));
+					tbResumeEducation.setTbreLocationFormatted(getJson(jsonElement, "location.formatted"));
+					tbResumeEducation.setTbreLocationCity(getJson(jsonElement, "location.city"));
+					tbResumeEducation.setTbreLocationState(getJson(jsonElement, "location.state"));
+					tbResumeEducation.setTbreLocationCountry(getJson(jsonElement, "location.country"));
+					tbResumeEducation.setTbreLocationRawInput(getJson(jsonElement, "location.rawInput"));
+					tbResumeEducation.setTbreLocationCountryCode(getJson(jsonElement, "location.countryCode"));
+					tbResumeEducation.setTbreLocationLatitude(getJson(jsonElement, "location.latitude"));
+					tbResumeEducation.setTbreLocationLongitude(getJson(jsonElement, "location.longitude"));				
+					tbResumeEducation.setTbreStartDate(getJsonDate(getJson(jsonElement, "startDate")));
+					tbResumeEducation.setTbreCompletionDate(getJsonDate(getJson(jsonElement, "completionDate")));
+					tbResumeEducation.setTbreIsCurrent(getJson(jsonElement, "isCurrent"));
+					tbResumeEducationRepository.save(tbResumeEducation);
+				}
+
+				String jsonWorkExperience = tbResume.getTbrDataWorkExperience();
+				JsonArray jsonArrayWorkExperience = JsonParser.parseString(jsonWorkExperience).getAsJsonArray();
+				for (JsonElement jsonElement : jsonArrayWorkExperience) {
+					TbResumeWorkExperience tbResumeWorkExperience = new TbResumeWorkExperience();
+					tbResumeWorkExperience.setTbrweUuid(new Uid().generateString(5));
+					tbResumeWorkExperience.setTbrweCreateId(optTbUser.get().getTbuId());
+					tbResumeWorkExperience.setTbrweCreateIdc(optTbUser.get().getTbuCreateIdc());
+					tbResumeWorkExperience.setTbrweCreateDate(new Date());
+					tbResumeWorkExperience.setTbrweStatus(TbResumeWorkExperienceRepository.Active);
+					tbResumeWorkExperience.setTbrId(tbResume.getTbrId());
+					tbResumeWorkExperience.setTbrweJobTitle(getJson(jsonElement, "jobTitle"));
+					tbResumeWorkExperience.setTbrweJobTitleNormalized(getJson(jsonElement, "occupation.jobTitleNormalized"));
+					tbResumeWorkExperience.setTbrweOrganization(getJson(jsonElement, "organization"));
+					tbResumeWorkExperience.setTbrweStartDate(getJsonDate(getJson(jsonElement, "dates.startDate")));
+					tbResumeWorkExperience.setTbrweEndDate(getJsonDate(getJson(jsonElement, "dates.endDate")));
+					tbResumeWorkExperience.setTbrweMonthsInPosition(getJsonInteger(jsonElement, "dates.monthsInPosition"));
+					tbResumeWorkExperience.setTbrweIsCurrent(getJson(jsonElement, "dates.isCurrent"));
+					tbResumeWorkExperience.setTbrweJobDescription(getJson(jsonElement, "jobDescription"));
+					tbResumeWorkExperience.setTbrweMinorGroup(getJson(jsonElement, "occupation.classification.minorGroup"));
+					tbResumeWorkExperience.setTbrweMajorGroup(getJson(jsonElement, "occupation.classification.majorGroup"));
+					tbResumeWorkExperience.setTbrweSubMajorGroup(getJson(jsonElement, "occupation.classification.subMajorGroup"));
+					tbResumeWorkExperience.setTbrweManagementLevel(getJson(jsonElement, "occupation.managementLevel"));
+					tbResumeWorkExperienceRepository.save(tbResumeWorkExperience);
+				}
+
+				String jsonSkills = tbResume.getTbrDataSkills();
+				JsonArray jsonArraySkills = JsonParser.parseString(jsonSkills).getAsJsonArray();
+				for (JsonElement jsonElement : jsonArraySkills) {
+					TbResumeSkill tbResumeSkill = new TbResumeSkill();
+					tbResumeSkill.setTbrsUuid(new Uid().generateString(5));
+					tbResumeSkill.setTbrsCreateId(optTbUser.get().getTbuId());
+					tbResumeSkill.setTbrsCreateIdc(optTbUser.get().getTbuCreateIdc());
+					tbResumeSkill.setTbrsCreateDate(new Date());
+					tbResumeSkill.setTbrsStatus(TbResumeSkillRepository.Active);
+					tbResumeSkill.setTbrId(tbResume.getTbrId());
+					tbResumeSkill.setTbrsName(getJson(jsonElement, "name"));
+					tbResumeSkill.setTbrsLastUsed(getJsonDate(getJson(jsonElement, "lastUsed")));
+					tbResumeSkill.setTbrsNumberOfMonths(getJsonInteger(jsonElement, "numberOfMonths"));
+					tbResumeSkill.setTbrsType(getJson(jsonElement, "type"));
+					tbResumeSkillRepository.save(tbResumeSkill);
+				}
+
+				String jsonCertifications = tbResume.getTbrDataCertifications();
+				JsonArray jsonArrayCertifications = JsonParser.parseString(jsonCertifications).getAsJsonArray();
+				for (JsonElement jsonElement : jsonArrayCertifications) {
+					TbResumeCertification tbResumeCertification = new TbResumeCertification();
+					tbResumeCertification.setTbrcUuid(new Uid().generateString(5));
+					tbResumeCertification.setTbrcCreateId(optTbUser.get().getTbuId());
+					tbResumeCertification.setTbrcCreateIdc(optTbUser.get().getTbuCreateIdc());
+					tbResumeCertification.setTbrcCreateDate(new Date());
+					tbResumeCertification.setTbrcStatus(TbResumeCertificationRepository.Active);
+					tbResumeCertification.setTbrId(tbResume.getTbrId());
+					tbResumeCertification.setTbrcName(jsonElement.getAsString());				
+					tbResumeCertificationRepository.save(tbResumeCertification);
+				}	
 
 				TbJobResume tbJobResume = new TbJobResume();
 				tbJobResume.setTbjrCreateId(optTbUser.get().getTbuId());
@@ -320,7 +440,7 @@ public class ResumeService {
 			TbResume exampleTbResume = new TbResume();
 			exampleTbResume.setTbrUuid(tbjUuid);
 			exampleTbResume.setTbrCreateId(optTbUser.get().getTbuCreateId());
-			Optional<TbResume> optTbResume = tbResumeRepository.findOne(Example.of(exampleTbResume));
+			Optional<TbResume> optTbResume = tbResumeRepository.findOne(Example.of(exampleTbResume));					
 			
 			if (optTbResume.isPresent()) {
 				responseModel.setTbResume(optTbResume.get());
@@ -333,5 +453,66 @@ public class ResumeService {
 		}
 		
 		return responseModel;
+	}
+
+	private Integer getJsonInteger(JsonElement jsonElement, String path) {
+		try {
+			String[] paths = path.split("\\.");
+			for (String p : paths) {
+				jsonElement = jsonElement.getAsJsonObject().get(p);
+				if (jsonElement == null) {
+					return null;
+				}
+			}
+			return jsonElement.getAsInt();
+		} catch (Exception e) {
+			log.error(e.getMessage());
+			return null;
+		}
+	}
+
+	private Date getJsonDate(String date) {
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+			return sdf.parse(date);
+		} catch (Exception e) {
+			log.error(e.getMessage());
+			return null;
+		}
+	}
+
+	private String getJson(JsonElement jsonElement, String path) {
+		try {
+			String[] paths = path.split("\\.");
+			for (String p : paths) {
+				jsonElement = jsonElement.getAsJsonObject().get(p);
+				if (jsonElement == null) {
+					return null;
+				}
+			}
+			return jsonElement.getAsString();
+		} catch (Exception e) {
+			log.error(e.getMessage());
+			return null;
+		}		
+	}
+
+	private String getJsonArray(String jsonData) {
+		try {
+			Gson gson = new Gson();
+			String[] data = gson.fromJson(jsonData, String[].class);
+			String result = "";
+			for (String d : data) {
+				if (result.equals("")) {
+					result = d;
+				} else {
+					result = result + ", " + d;
+				}			
+			}
+			return result;
+		} catch (Exception e) {
+			log.error(e.getMessage());
+			return jsonData;
+		}
 	}
 }
