c0ead2b29b1addc2162cb95a5bcb661b7537c2de
Achmad Amri <achmad.amri@gmail.com>
2023-05-17 12:05:36 +0700

    Update

--- bsd-api-rec-departments/src/main/java/com/api/rec/departments/db/repository/ViewResumeJobRepository.java
+++ bsd-api-rec-departments/src/main/java/com/api/rec/departments/db/repository/ViewResumeJobRepository.java
@@ -9,27 +9,27 @@ import org.springframework.data.jpa.repository.Query;
 import com.api.rec.departments.db.entity.ViewResumeJob;
 
 public interface ViewResumeJobRepository extends JpaRepository<ViewResumeJob, Integer> {
-	@Query(value = "SELECT * FROM view_resume_job WHERE tbr_status = 'active' and tbr_create_idc = ?1 and tbj_id = ?2 or tbj_id is null", nativeQuery = true)
+	@Query(value = "SELECT * FROM view_resume_job WHERE tbr_create_idc = ?1 and tbj_id = ?2 or tbj_id is null", nativeQuery = true)
 	List<ViewResumeJob> find(Integer tbrCreateIdc, Integer tbjId, Pageable pageable);
 
-	@Query(value = "SELECT count(0) FROM view_resume_job WHERE tbr_status = 'active' and tbr_create_idc = ?1 and (tbj_id = ?2 or tbj_id is null)", nativeQuery = true)
+	@Query(value = "SELECT count(0) FROM view_resume_job WHERE tbr_create_idc = ?1 and (tbj_id = ?2 or tbj_id is null)", nativeQuery = true)
 	Long count(Integer tbrCreateIdc, Integer tbjId);
 
-	@Query(value = "SELECT * FROM view_resume_job WHERE tbr_status = 'active' and tbr_create_idc = ?1 and tbj_id = ?2 and tbr_uuid like %?3% and tbr_data_name_raw like %?4% and tbr_status like %?5% and tbr_assigned like %?6%", nativeQuery = true)
+	@Query(value = "SELECT * FROM view_resume_job WHERE tbr_create_idc = ?1 and tbj_id = ?2 and tbr_uuid like %?3% and tbr_data_name_raw like %?4% and tbr_status like %?5% and tbr_assigned like %?6%", nativeQuery = true)
 	List<ViewResumeJob> findAssigned(Integer tbrCreateIdc, Integer tbjId, String tbrUuid, String tbrDataNameRaw, String tbrStatus, String tbrAssigned, Pageable pageable);
 
-	@Query(value = "SELECT count(0) FROM view_resume_job WHERE tbr_status = 'active' and tbr_create_idc = ?1 and tbj_id = ?2 and tbr_uuid like %?3% and tbr_data_name_raw like %?4% and tbr_status like %?5% and tbr_assigned like %?6%", nativeQuery = true)
+	@Query(value = "SELECT count(0) FROM view_resume_job WHERE tbr_create_idc = ?1 and tbj_id = ?2 and tbr_uuid like %?3% and tbr_data_name_raw like %?4% and tbr_status like %?5% and tbr_assigned like %?6%", nativeQuery = true)
 	Long countAssigned(Integer tbrCreateIdc, Integer tbjId, String tbrUuid, String tbrDataNameRaw, String tbrStatus, String tbrAssigned);
 
-	@Query(value = "SELECT * FROM view_resume_job WHERE tbr_status = 'active' and tbr_create_idc = ?1 and tbj_id is null and tbr_uuid like %?2% and tbr_data_name_raw like %?3% and tbr_status like %?4% and tbr_assigned like %?5%", nativeQuery = true)
+	@Query(value = "SELECT * FROM view_resume_job WHERE tbr_create_idc = ?1 and tbj_id is null and tbr_uuid like %?2% and tbr_data_name_raw like %?3% and tbr_status like %?4% and tbr_assigned like %?5%", nativeQuery = true)
 	List<ViewResumeJob> findNotAssigned(Integer tbrCreateIdc, String tbrUuid, String tbrDataNameRaw, String tbrStatus, String tbrAssigned, Pageable pageable);
 
-	@Query(value = "SELECT count(0) FROM view_resume_job WHERE tbr_status = 'active' and tbr_create_idc = ?1 and tbj_id is null and tbr_uuid like %?2% and tbr_data_name_raw like %?3% and tbr_status like %?4% and tbr_assigned like %?4%", nativeQuery = true)
+	@Query(value = "SELECT count(0) FROM view_resume_job WHERE tbr_create_idc = ?1 and tbj_id is null and tbr_uuid like %?2% and tbr_data_name_raw like %?3% and tbr_status like %?4% and tbr_assigned like %?4%", nativeQuery = true)
 	Long countNotAssigned(Integer tbrCreateIdc, String tbrUuid, String tbrDataNameRaw, String tbrStatus, String tbrAssigned);
 
-	@Query(value = "SELECT * FROM view_resume_job WHERE tbr_status = 'active' and tbr_create_idc = ?1 and tbr_uuid like %?2% and tbr_data_name_raw like %?3%", nativeQuery = true)
+	@Query(value = "SELECT * FROM view_resume_job WHERE tbr_create_idc = ?1 and tbr_uuid like %?2% and tbr_data_name_raw like %?3%", nativeQuery = true)
 	List<ViewResumeJob> findList(Integer tbrCreateIdc, String tbrUuid, String tbrDataNameRaw, Pageable pageable);
 
-	@Query(value = "SELECT count(0) FROM view_resume_job WHERE tbr_status = 'active' and tbr_create_idc = ?1 and tbr_uuid like %?2% and tbr_data_name_raw like %?3%", nativeQuery = true)
+	@Query(value = "SELECT count(0) FROM view_resume_job WHERE tbr_create_idc = ?1 and tbr_uuid like %?2% and tbr_data_name_raw like %?3%", nativeQuery = true)
 	Long countList(Integer tbrCreateIdc, String tbrUuid, String tbrDataNameRaw);
 }
